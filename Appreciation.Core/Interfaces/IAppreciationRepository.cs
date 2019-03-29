e392252dae9e9e20892a41ef3493839e7c624b5e
Stephen Donaghy <t3h.kitteh@gmail.com>
2019-03-29 19:14:30 +0000

    Added Appreciation.Core project with some basic models

---
+++ Appreciation.Core/Interfaces/IAppreciationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Appreciation.Core.Interfaces
+{
+    public interface IAppreciationRepository
+    {
+        List<Models.Appreciation> GetAppreciations(DateTime from, DateTime to);
+    }
+}
---
+++ Appreciation.Core/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appreciation.Core.Models
+{
+    public class Person
+    {
+        public string Name;
+        public int NumberOfAppreciationsGivenTotal;
+        public int NumberOfAppreciationsReceivedTotal;
+    }
+}
